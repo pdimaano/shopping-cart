1e27d356b832518194fe3c915d5cd206df095a37
Philip Dimaano <pdimaano7@gmail.com>
2022-04-07 14:09:46 -0700

    initial shopping components

---
+++ src/components/Homepage.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Homepage = () => {
+    return (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <h1
+                style={{
+                    display: "flex",
+                    color: "black",
+                    marginTop: "200px",
+                    fontFamily: "TITLE",
+                }}
+            >
+                A One Stop Shop
+            </h1>
+        </div>
+    );
+};
+
+export default Homepage;
---
+++ src/components/Shop.js
@@ -0,0 +1,86 @@
+import { React, useEffect, useState } from "react";
+import { Card, Button, FormControl } from "react-bootstrap";
+
+const Shop = () => {
+    const [cart, setCart] = useState([]);
+    const [cartClicked, setCartClicked] = useState(false);
+    const [cartLength, setCartLength] = useState(cart.length);
+
+    function addToCart(e, food) {
+        for (let i = 0; i < e.target.nextElementSibling.value; i++) {
+            setCart((prevCart) => [...prevCart, food]);
+        }
+    }
+
+    function emptyCart() {
+        setCart([]);
+        setCartClicked(0);
+    }
+
+    useEffect(() => {
+        setCartLength(cart.length);
+    }, [cart]);
+
+    return (
+        <div>
+            {cartClicked ? (
+                <Checkout
+                    cartLength={cartLength}
+                    cart={cart}
+                    setCartClicked={setCartClicked}
+                    emptyCart={emptyCart}
+                />
+            ) : null}
+            <div
+                className="shop-page"
+            >
+                <div
+                    onClick={() => {
+                        setCartClicked(!cartClicked);
+                    }}
+                    className="cart-icon"
+                >
+                    <span>{cart.length}</span>
+                    <h3>CART</h3>
+                </div>
+                {food.map((food) => {
+                    return (
+                        <Card className="food-container" key={food.id} id={food.id}>
+                            <Card.Img variant="top" src={food.image} />
+                            <Card.Body style={{ position: "relative", bottom: "80px" }}>
+                                <h1>{food.name}</h1>
+                                <Card.Text>{food.description}</Card.Text>
+                                <div style={{ display: "flex", gap: "10px" }}>
+                                    <Button
+                                        className="add-to-cart"
+                                        onClick={(e) => addToCart(e, food)}
+                                    >
+                                        Add to Cart
+                                    </Button>
+                                    <FormControl
+                                        min="0"
+                                        className="amount-input"
+                                        style={{ width: "55px" }}
+                                        type="number"
+                                        defaultValue="1"
+                                    ></FormControl>
+                                </div>
+                            </Card.Body>
+                            <div>
+                                <div
+                                    style={{
+                                        height: "1px",
+                                        width: "100%",
+                                        background: "white",
+                                    }}
+                                ></div>
+                            </div>
+                        </Card>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
