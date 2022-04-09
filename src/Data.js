696e8d31c2eb744889f7d5c61ce676ed052c1179
Philip Dimaano <pdimaano7@gmail.com>
2022-04-09 14:41:01 -0700

    home image & price changes

--- src/Data.js
+++ src/Data.js
@@ -5,7 +5,7 @@ const food = [
         name: "Pocky",
         image:
             "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Pocky-Sticks.jpg/1600px-Pocky-Sticks.jpg?20120427033344",
-        price: "5",
+        price: "3",
         description:
             "Strawberry, chocolate or matcha-flavored",
     },
@@ -15,7 +15,7 @@ const food = [
         name: "White Rabbit",
         image:
             "https://upload.wikimedia.org/wikipedia/commons/c/c7/Tin_of_White_Rabbit_Sweets.jpg?20060114061204",
-        price: "5",
+        price: "2",
         description:
             "A sweet taste of nostalgia in every wrapper",
     },
@@ -25,7 +25,7 @@ const food = [
         name: "Shrimp Chips",
         image:
             "https://upload.wikimedia.org/wikipedia/commons/0/0c/Kappa_Ebisen_%28Calbee%29.jpg?20070911092847",
-        price: "5",
+        price: "3",
         description:
             "A light and salty seafood-flavored staple",
     },
@@ -35,7 +35,7 @@ const food = [
         name: "Ramen",
         image:
             "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Instant_noodles_at_home.jpg/1600px-Instant_noodles_at_home.jpg?20200731111744",
-        price: "5",
+        price: "3",
         description:
             "Beef or chicken-flavored",
     },
@@ -45,7 +45,7 @@ const food = [
         name: "Yan Yan",
         image:
             "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Yan_Yan_%28snack%29.JPG/900px-Yan_Yan_%28snack%29.JPG?20060329000215",
-        price: "5",
+        price: "4",
         description:
             "Vanilla, chocolate or strawberry-flavored",
     },
@@ -55,7 +55,7 @@ const food = [
         name: "Cream Wafer",
         image:
             "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Pink_Wafers.jpg/1599px-Pink_Wafers.jpg?20210213164852",
-        price: "5",
+        price: "4",
         description:
             "Vanilla, chocolate or strawberry-flavored",
     },
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,10 @@
 const Homepage = () => {
     return (
-        <div style={{ display: "flex", justifyContent: "center", background: "#93cee3" }}>
+        <div style={{ 
+                display: "flex", 
+                flexDirection: "column",
+                alignItems: "center",
+                background: "#93cee3" }}>
             <h1
                 style={{
                     display: "flex",
@@ -11,6 +15,15 @@ const Homepage = () => {
             >
                 A One Stop Shop
             </h1>
+            <img
+                style={{
+                    width: "600px",
+                    marginTop: "45px",
+                    border: "2px solid black",
+                }}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Sihanoukville_-_Hawaii_beach%2C_beach_house.jpg/1599px-Sihanoukville_-_Hawaii_beach%2C_beach_house.jpg?20190521203607"
+                alt="beach shack"
+            />
         </div>
     );
 };
